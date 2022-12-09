07b8c88191e5177aafe384baf896436580f6c750
Sam Chen <sam8211112222@gmail.com>
2022-12-09 15:11:29 +0800

    extract mapRow to a function

--- src/CSVReader.ts
+++ src/CSVReader.ts
@@ -16,16 +16,17 @@ export class CSVReader {
         })
             .split('\n')
             .map((row: string): string[] => row.split(','))
-            .map((row: string[]): MatchData => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    row[5] as MatchResult,
-                    row[6],
-                ]
-            })
+            .map(this.mapRow)
+    }
+    mapRow(row: string[]): MatchData {
+        return [
+            dateStringToDate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6],
+        ]
     }
 }
